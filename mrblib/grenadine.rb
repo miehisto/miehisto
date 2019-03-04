5cd13d152a82e4f372e297f48a55be0a6ad9dbb0
Uchio Kondo <udzura@udzura.jp>
2019-03-04 23:44:59 +0900

    Add mock cli interface

--- mrblib/grenadine.rb
+++ mrblib/grenadine.rb
@@ -9,6 +9,17 @@ def __main__(argv)
     Grenadine::Container.run(argv[1..-1])
   when "daemon"
     Grenadine::Container.spawn(argv[1..-1])
+  # Implement below!
+  when "dump"
+    Grenadine::Container.dump(argv[1..-1])
+  when "list", "ls"
+    Grenadine::Container.list(argv[1..-1])
+  when "version"
+    puts "grenadine: v#{Grenadine::VERSION}"
+  when "restore"
+    Grenadine::Container.restore(argv[1..-1])
+  when "help"
+    puts "...help"
   else
     raise "Invalid subcommand: #{argv[0]}"
   end
